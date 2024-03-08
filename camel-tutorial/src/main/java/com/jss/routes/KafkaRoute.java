172ff90baf20241e815748f7070ea7d4dacc7d7e
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2024-03-08 22:35:33 +0530

    Kafka route fully implemented

--- camel-tutorial/src/main/java/com/jss/routes/KafkaRoute.java
+++ camel-tutorial/src/main/java/com/jss/routes/KafkaRoute.java
@@ -3,23 +3,25 @@ package com.jss.routes;
 import org.apache.camel.builder.RouteBuilder;
 
 public class KafkaRoute extends RouteBuilder {
-
-
-
     @Override
     public void configure() throws Exception {
-       from("file:/DEVELOPMENT/orders")
-               .to("kafka://orders?brokers=kafka:9092");
 
-       from("kafka://orders?brokers=kafka:9092")
-                .log("${body}")
-                .split(xpath("/orders/order"))
-                .choice()
-                .when(xpath("//order/type/text() = 'vegetables'"))
-                .to("activemq:queue:vegetable")
-                .when(xpath("//order/type/text() = 'fruits'"))
-                .to("activemq:queue:fruits")
-                .otherwise()
-                .to("kafka://product?brokers=kafka:9092");
     }
+//
+//    @Override
+//    public void configure() throws Exception {
+//       from("file:/DEVELOPMENT/orders")
+//               .to("kafka://orders?brokers=kafka:9092");
+//
+//       from("kafka://orders?brokers=kafka:9092")
+//                .log("${body}")
+//                .split(xpath("/orders/order"))
+//                .choice()
+//                .when(xpath("//order/type/text() = 'vegetables'"))
+//                .to("activemq:queue:vegetable")
+//                .when(xpath("//order/type/text() = 'fruits'"))
+//                .to("activemq:queue:fruits")
+//                .otherwise()
+//                .to("kafka://product?brokers=kafka:9092");
+//    }
 }
--- camel-tutorial/src/main/java/com/jss/routes/WeatherRoute.java
+++ camel-tutorial/src/main/java/com/jss/routes/WeatherRoute.java
@@ -10,31 +10,35 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-import static com.jss.config.CamelConfiguration.RABBIT_URI;
-import static org.apache.camel.LoggingLevel.ERROR;
+//import static com.jss.config.CamelConfiguration.RABBIT_URI;
+//import static org.apache.camel.LoggingLevel.ERROR;
 
 @Component
 public class WeatherRoute extends RouteBuilder {
-
     @Override
     public void configure() throws Exception {
-        fromF(RABBIT_URI, "weather", "weather")
-                .log(ERROR, "Before Enrichment: ${body}")
-                .unmarshal().json(JsonLibrary.Jackson, WeatherDto.class)
-                .process(this::enrichWeatherDto)
-                .log(ERROR, "After Enrichment: ${body}")
-                .marshal().json(JsonLibrary.Jackson, WeatherDto.class)
-                .toF(RABBIT_URI, "weather-events", "weather-events")
-                .to("file:///Users/jasvinder.saggu/projects/temp/camel-demos/?fileName=weather-events.txt&fileExist=Append")
-        ;
+
     }
 
-    private void enrichWeatherDto(Exchange exchange) {
-        WeatherDto dto = exchange.getMessage().getBody(WeatherDto.class);
-        dto.setReceivedTime(new Date().toString());
-
-        Message message = new DefaultMessage(exchange);
-        message.setBody(dto);
-        exchange.setMessage(message);
-    }
+//    @Override
+//    public void configure() throws Exception {
+//        fromF(RABBIT_URI, "weather", "weather")
+//                .log(ERROR, "Before Enrichment: ${body}")
+//                .unmarshal().json(JsonLibrary.Jackson, WeatherDto.class)
+//                .process(this::enrichWeatherDto)
+//                .log(ERROR, "After Enrichment: ${body}")
+//                .marshal().json(JsonLibrary.Jackson, WeatherDto.class)
+//                .toF(RABBIT_URI, "weather-events", "weather-events")
+//                .to("file:///Users/jasvinder.saggu/projects/temp/camel-demos/?fileName=weather-events.txt&fileExist=Append")
+//        ;
+//    }
+//
+//    private void enrichWeatherDto(Exchange exchange) {
+//        WeatherDto dto = exchange.getMessage().getBody(WeatherDto.class);
+//        dto.setReceivedTime(new Date().toString());
+//
+//        Message message = new DefaultMessage(exchange);
+//        message.setBody(dto);
+//        exchange.setMessage(message);
+//    }
 }
